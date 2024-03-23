ce08c32299530c7706d5ba68d0e243ac415ba337
Michael Kopp <kopp.michael@yahoo.de>
2024-03-23 19:54:05 +0100

    add some tests

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,35 @@ impl PartialEq for Point {
     }
 }
 
+#[cfg(test)]
+mod test4 {
+    use super::*;
+    use std::hash::{DefaultHasher, Hash, Hasher};
+
+    fn get_hash<T: Hash>(t: &T) -> u64 {
+        let mut s = DefaultHasher::new();
+        t.hash(&mut s);
+        s.finish()
+    }
+
+    #[test]
+    fn test_points_equal() {
+        let point1 = Point { x: 1, y: 2 };
+        let point2 = Point { x: 1, y: 2 };
+        let point3 = Point { x: 1, y: 3 };
+        assert_eq!(point1, point2);
+        assert_eq!(
+            get_hash(&point1),
+            get_hash(&point2),
+            "hashes of equal objects should be equal."
+        );
+        assert_eq!(point1 == point2, true);
+        assert_ne!(point1, point3);
+        assert_ne!(get_hash(&point1), get_hash(&point3));
+        assert_eq!(point1 == point3, false);
+    }
+}
+
 // Central square is an actual 2x2 square.
 // Positions where an occupancy can be are indicated by x,y coordinates.
 //
@@ -60,7 +89,7 @@ impl Shape {
     fn get_points(&self) -> Vec<Point> {
         match self {
             Shape::Free => vec![],
-            Shape::M1 => vec![Point { x: 0, y: 0 }],
+            Shape::M1 => vec![Point { x: 1, y: 0 }],
             Shape::M2 => vec![Point { x: 0, y: 0 }, Point { x: 1, y: 1 }],
             Shape::M3 => vec![
                 Point { x: 0, y: 0 },
@@ -103,6 +132,7 @@ impl PartialEq for BoardIndex2d {
 }
 
 impl BoardIndex2d {
+    /// index to index the array of shapes
     fn to_index(&self) -> usize {
         (self.y * 3 + self.x) as usize
     }
@@ -112,6 +142,8 @@ impl BoardIndex2d {
             y: (index / 3) as i32,
         }
     }
+
+    /// Returns the neighbor in the given direction, if it exists.
     fn neighbor(&self, direction: &Direction) -> Option<BoardIndex2d> {
         let naive_index = match direction {
             Direction::Up => BoardIndex2d {
@@ -149,6 +181,32 @@ fn has_duplicates<T: std::hash::Hash + std::cmp::Eq>(vec: &[T]) -> bool {
     false
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_has_duplicates() {
+        // Test case with duplicates
+        let vec_with_duplicates = vec![1, 2, 3, 2, 4];
+        assert_eq!(has_duplicates(&vec_with_duplicates), true);
+
+        // Test case without duplicates
+        let vec_without_duplicates = vec![1, 2, 3, 4, 5];
+        assert_eq!(has_duplicates(&vec_without_duplicates), false);
+
+        // Test case with empty vector
+        let empty_vec: Vec<i32> = vec![];
+        assert_eq!(has_duplicates(&empty_vec), false);
+
+        // Test case with a struct
+        let mut points = vec![Point { x: 1, y: 2 }, Point { x: 3, y: 4 }];
+        assert_eq!(has_duplicates(&points), false);
+        points.push(points[0].clone());
+        assert_eq!(has_duplicates(&points), true);
+    }
+}
+
 /// Describe how the tile at `board_index` moves on the grid in direction `grid_dx, grid_dy`.
 struct MovingTile {
     board_index: BoardIndex2d,
@@ -164,35 +222,132 @@ impl MovingTile {
             grid_dy: 0,
         }
     }
+}
 
-    fn map_shape_points_to_grid_points(
-        &self,
-        shape: &Shape,
-        board_index: &BoardIndex2d,
-        grid_x: &i32,
-        grid_y: &i32,
-    ) -> Vec<Point> {
-        let dx: i32;
-        let dy: i32;
-        if *board_index == self.board_index {
-            dx = self.grid_dx;
-            dy = self.grid_dy;
-        } else {
-            dx = 0;
-            dy = 0;
-        }
-        shape
-            .get_points()
-            .iter()
-            .map(|point| Point {
-                x: point.x + grid_x + dx,
-                y: point.y + grid_y + dy,
-            })
-            .collect()
+/// convert to the grid coordinates that are used for drawing/collision detection
+fn grid_coordinates_of(board_index: &BoardIndex2d) -> Point {
+    Point {
+        x: 1 + 2 * board_index.x,
+        y: 1 + 2 * board_index.y,
+    }
+}
+
+fn map_shape_points_to_grid_points(
+    shape: &Shape,
+    moving_tile: &MovingTile,
+    board_index: &BoardIndex2d,
+) -> Vec<Point> {
+    let dx: i32;
+    let dy: i32;
+    if *board_index == moving_tile.board_index {
+        dx = moving_tile.grid_dx;
+        dy = moving_tile.grid_dy;
+    } else {
+        dx = 0;
+        dy = 0;
+    }
+
+    let grid_coordinates = grid_coordinates_of(board_index);
+
+    shape
+        .get_points()
+        .iter()
+        .map(|point| Point {
+            x: point.x + grid_coordinates.x + dx,
+            y: point.y + grid_coordinates.y + dy,
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests2 {
+    use itertools::Itertools;
+
+    use super::*;
+
+    #[test]
+    fn test_map_shape_points_to_grid_points() {
+        let mut board = Board::empty_board();
+        let top_left = BoardIndex2d { x: 0, y: 0 };
+        let shape = Shape::M1;
+        board.shapes[top_left.to_index()] = shape.clone();
+        let local_points = shape.get_points();
+
+        let moving_top_left_tile = MovingTile {
+            board_index: top_left.clone(),
+            grid_dx: 0,
+            grid_dy: 0,
+        };
+
+        assert_eq!(
+            map_shape_points_to_grid_points(&shape, &moving_top_left_tile, &top_left),
+            local_points
+                .iter()
+                .map(|point| Point {
+                    x: point.x + 1,
+                    y: point.y + 1
+                })
+                .collect_vec()
+        );
+
+        let center = BoardIndex2d { x: 1, y: 1 };
+
+        assert_eq!(
+            map_shape_points_to_grid_points(&shape, &moving_top_left_tile, &center),
+            local_points
+                .iter()
+                .map(|point| Point {
+                    x: point.x + 3,
+                    y: point.y + 3
+                })
+                .collect_vec()
+        );
+
+        // now apply some dx
+
+        let dx = 2;
+
+        let moving_center_tile = MovingTile {
+            board_index: center.clone(),
+            grid_dx: dx,
+            grid_dy: 0,
+        };
+
+        // global points move accordingly
+
+        assert_eq!(
+            map_shape_points_to_grid_points(&shape, &moving_center_tile, &center),
+            local_points
+                .iter()
+                .map(|point| Point {
+                    x: point.x + 3 + dx,
+                    y: point.y + 3
+                })
+                .collect_vec()
+        );
+
+        // unless the moving tile is a differnt tile, then they stay the same
+
+        assert_eq!(
+            map_shape_points_to_grid_points(&shape, &moving_top_left_tile, &center),
+            local_points
+                .iter()
+                .map(|point| Point {
+                    x: point.x + 3,
+                    y: point.y + 3
+                })
+                .collect_vec()
+        );
     }
 }
 
 impl Board {
+    fn empty_board() -> Board {
+        Board {
+            shapes: [Shape::Free; 9],
+        }
+    }
+
     fn is_valid(&self) -> bool {
         // has exactly one free space
         let mut free_count = 0;
@@ -204,7 +359,7 @@ impl Board {
         }
 
         // and has no collissions
-        free_count == 1 && self.is_collission_free(MovingTile::no_move())
+        free_count == 1 && self.is_collission_free(&MovingTile::no_move())
     }
 
     fn find_free_space(&self) -> BoardIndex2d {
@@ -231,14 +386,13 @@ impl Board {
         }
     }
 
-    fn is_collission_free(&self, moving_tile: MovingTile) -> bool {
+    fn is_collission_free(&self, moving_tile: &MovingTile) -> bool {
         let mut occupied_points: Vec<Point> = vec![];
         for y in 0..3 {
             for x in 0..3 {
                 let board_index = BoardIndex2d { x, y };
                 let shape = &self.shapes[board_index.to_index()];
-                moving_tile
-                    .map_shape_points_to_grid_points(shape, &board_index, &x, &y)
+                map_shape_points_to_grid_points(shape, &moving_tile, &board_index)
                     .into_iter()
                     .for_each(|point| occupied_points.push(point));
             }
@@ -246,6 +400,22 @@ impl Board {
         !has_duplicates(&occupied_points[..])
     }
 }
+
+#[cfg(test)]
+mod test3 {
+    use super::*;
+
+    #[test]
+    fn test_is_collission_free() {
+        let mut board = super::Board::empty_board();
+        board.shapes[0] = Shape::Ship;
+        board.shapes[1] = Shape::Ship;
+
+        assert!(!board.is_collission_free(&MovingTile::no_move()));
+    }
+}
+
+// drawing --------------------------------------------------------------------
 
 fn drawing_character_for(shape: &Shape) -> &str {
     match shape {
