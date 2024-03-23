fb0e4c6940c257cde5a3a0ef8e2c63bd6e8146f2
Michael Kopp <kopp.michael@yahoo.de>
2024-03-23 08:05:43 +0100

    WIP: implement collission check

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use crossterm::{
     event::{read, Event, KeyCode},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
+use std::collections::HashSet;
 use std::fmt;
 use std::fmt::Display;
 
@@ -16,9 +17,16 @@ enum Shape {
     Ship,
 }
 
+#[derive(Debug, Clone, Copy, Hash, Eq)]
 struct Point {
     x: i32,
     y: i32,
+}
+
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
 }
 
 // Central square is an actual 2x2 square.
@@ -82,9 +90,16 @@ enum Direction {
     Right,
 }
 
+#[derive(Debug, Clone, Copy, Hash, Eq)]
 struct BoardIndex2d {
     x: i32,
     y: i32,
+}
+
+impl PartialEq for BoardIndex2d {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
 }
 
 impl BoardIndex2d {
@@ -124,8 +139,62 @@ impl BoardIndex2d {
     }
 }
 
+fn has_duplicates<T: std::hash::Hash + std::cmp::Eq>(vec: &[T]) -> bool {
+    let mut seen = HashSet::new();
+    for value in vec {
+        if !seen.insert(value) {
+            return true;
+        }
+    }
+    false
+}
+
+/// Describe how the tile at `board_index` moves on the grid in direction `grid_dx, grid_dy`.
+struct MovingTile {
+    board_index: BoardIndex2d,
+    grid_dx: i32,
+    grid_dy: i32,
+}
+
+impl MovingTile {
+    fn no_move() -> MovingTile {
+        MovingTile {
+            board_index: BoardIndex2d { x: 0, y: 0 },
+            grid_dx: 0,
+            grid_dy: 0,
+        }
+    }
+
+    fn map_shape_points_to_grid_points(
+        &self,
+        shape: &Shape,
+        board_index: &BoardIndex2d,
+        grid_x: &i32,
+        grid_y: &i32,
+    ) -> Vec<Point> {
+        let dx: i32;
+        let dy: i32;
+        if *board_index == self.board_index {
+            dx = self.grid_dx;
+            dy = self.grid_dy;
+        } else {
+            dx = 0;
+            dy = 0;
+        }
+        shape
+            .get_points()
+            .iter()
+            .map(|point| Point {
+                x: point.x + grid_x + dx,
+                y: point.y + grid_y + dy,
+            })
+            .collect()
+    }
+}
+
 impl Board {
     fn is_valid(&self) -> bool {
+        // has exactly one free space
         let mut free_count = 0;
 
         for shape in &self.shapes {
@@ -134,7 +203,8 @@ impl Board {
             }
         }
 
-        free_count == 1
+        // and has no collissions
+        free_count == 1 && self.is_collission_free(MovingTile::no_move())
     }
 
     fn find_free_space(&self) -> BoardIndex2d {
@@ -147,6 +217,7 @@ impl Board {
         panic!("No free space found");
     }
 
+    /// Move the free space in the given direction iff it is possible (i.e. a valid move).
     fn move_free_space(&self, direction: &Direction) -> Option<Board> {
         let free_space_position = self.find_free_space();
         if let Some(neighbor) = free_space_position.neighbor(direction) {
@@ -160,23 +231,19 @@ impl Board {
         }
     }
 
-    fn get_occupied_points(&self) -> Vec<Point> {
+    fn is_collission_free(&self, moving_tile: MovingTile) -> bool {
         let mut occupied_points: Vec<Point> = vec![];
         for y in 0..3 {
             for x in 0..3 {
                 let board_index = BoardIndex2d { x, y };
                 let shape = &self.shapes[board_index.to_index()];
-                shape
-                    .get_points()
-                    .iter()
-                    .map(|point| Point {
-                        x: point.x + 1 + 2 * x,
-                        y: point.y + 1 + 2 * y,
-                    })
+                moving_tile
+                    .map_shape_points_to_grid_points(shape, &board_index, &x, &y)
+                    .into_iter()
                     .for_each(|point| occupied_points.push(point));
             }
         }
-        occupied_points
+        !has_duplicates(&occupied_points[..])
     }
 }
 
@@ -250,6 +317,8 @@ fn main() -> crossterm::Result<()> {
     };
 
     println!("{}", board);
+
+    println!("Is valid: {}", board.is_valid());
 
     let mut history = vec![board];
 
